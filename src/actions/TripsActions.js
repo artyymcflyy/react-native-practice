d15eb98ae3dcca158343656447df2864ea04475c
Alec Breton <aleccbreton@gmail.com>
2017-09-27 21:13:02 -0400

    Alec's attempt at image overlay

--- src/actions/TripsActions.js
+++ src/actions/TripsActions.js
@@ -17,16 +17,108 @@ export const fetchTripsSuccess = ({ trips }) => {
 const payload = {
     data: {
         trips: [
-            { id: 1, name: 'Grocery Pizza' },
-            { id: 2, name: 'Gas' },
-            { id: 3, name: 'Corys House' },
-            { id: 4, name: 'Parents House' },
-            { id: 5, name: 'Grocery Shopping' },
-            { id: 6, name: 'Gas' },
-            { id: 7, name: 'Mount Major' },
-            { id: 8, name: 'Gas' },
-            { id: 9, name: 'Things for real' },
-            { id: 10, name: 'Gas' },
+            { 
+                "id": 1, 
+                "name": "Grocery Pizza",
+                "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    }
+                ] 
+            },
+            {  
+               "id":2,
+               "name":"Gas",
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    },
+                    {
+                        "img": 'https://www.biography.com/.image/c_fit%2Ccs_srgb%2Ch_406%2Cq_50%2Cw_620/MTE1ODA0OTcxMzI5MDk1MTgx/beyonce-knowles-39230-3-raw.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":3,
+               "name":'Corys House',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":4,
+               "name":'Parents House',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":5,
+               "name":'Grocery Shopping',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    },
+                    {
+                        "img": 'https://www.biography.com/.image/c_fit%2Ccs_srgb%2Ch_406%2Cq_50%2Cw_620/MTE1ODA0OTcxMzI5MDk1MTgx/beyonce-knowles-39230-3-raw.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":6,
+               "name":'Gas',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":7,
+               "name":'Mount Major',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    },
+                    {
+                        "img": 'https://www.biography.com/.image/c_fit%2Ccs_srgb%2Ch_406%2Cq_50%2Cw_620/MTE1ODA0OTcxMzI5MDk1MTgx/beyonce-knowles-39230-3-raw.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":8,
+               "name":'Gas',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":9,
+               "name":'Things for real',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    },
+                    {
+                        "img": 'https://www.biography.com/.image/c_fit%2Ccs_srgb%2Ch_406%2Cq_50%2Cw_620/MTE1ODA0OTcxMzI5MDk1MTgx/beyonce-knowles-39230-3-raw.jpg'
+                    }
+                ]
+            },
+            {  
+               "id":10,
+               "name":'Gas',
+               "users": [
+                    {
+                        "img": 'https://i.pinimg.com/736x/c7/28/ae/c728ae90470258962ecf92dc72201720--brad-pitt--square-faces.jpg'
+                    }
+                ]
+            }
         ]
     }
 };
